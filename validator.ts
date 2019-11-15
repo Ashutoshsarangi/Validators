2a86bae016a1cf5c03e97807feed0a999c815e6d
Ashu <ashutosh.softronet@gmail.com>
2019-11-15 08:24:56 +0530

    Done number and email field validator

--- validator.ts
+++ validator.ts
@@ -15,7 +15,7 @@ class Validator {
         }
     }
     /**
-     * This is a Name fied vaslidator which has Alphabets + Numeric
+     * This is a Name field validator which has Alphabets + Numeric
      * @param str is the String which we need to validate
      * @returns true/fase depending on the condition
      */
@@ -28,6 +28,35 @@ class Validator {
             return (false);
         }
     }
+    /**
+     * This is a Email field validator
+     * @param str is the String which we need to validate
+     * @returns true/fase depending on the condition
+     */
+    public emailFieldValidator = (str) => {
+        var letters = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (str.match(letters)) {
+            return (true);
+        }
+        else {
+            return (false);
+        }
+    }
+    /**
+     * This is a Number field validator
+     * @param str is the String which we need to validate
+     * @returns true/fase depending on the condition
+     */
+    public numberFieldValidator = (str) => {
+        var letters = /^[0-9]+$/;
+        if (str.match(letters)) {
+            return (true);
+        }
+        else {
+            return (false);
+        }
+    }
+
 }
 
 let v = new Validator();
